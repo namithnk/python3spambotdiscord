33925eb6b271358ca9affa5cf31a998e2cf3eead
namithnk <57763702+namithnk@users.noreply.github.com>
2021-04-07 14:06:56 +0530

    spam.py

---
+++ spam.py
@@ -0,0 +1,13 @@
+print("WARNING, STARTING NOW, YOU ONLY HAVE 5 SECONDS BEFORE THE BOT STARTS SPAMMING @everyone - unnianony1938")
+import pyautogui
+import time
+time.sleep(5)
+for i in range(10):
+    pyautogui.write("@everyone")
+    pyautogui.keyDown('enter')
+    pyautogui.keyUp('enter')
+    pyautogui.keyDown('enter')
+    pyautogui.keyUp('enter')
+pyautogui.write("get spammed boi")
+pyautogui.keyDown('enter')
+pyautogui.keyUp('enter')
